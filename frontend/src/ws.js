b492893d53ac1e69875e5ea611aa70c71234ffbd
Rahulkumar0407 <rahulsantukumar123@gmail.com>
2025-10-07 01:26:19 +0530

    fix: Correct syntax in ws.js

--- frontend/src/ws.js
+++ frontend/src/ws.js
@@ -1,6 +1,5 @@
 import { io } from 'socket.io-client';
 
 export function connectWS() {
-    // This uses the environment variable you set on Render
-    return io(import.meta.env.https://real-time-chat-app-um6p.onrender.com);
+  return io(import.meta.env.VITE_BACKEND_URL);
 }
